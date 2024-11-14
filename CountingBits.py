1df23e37da06362a896bab174c8be7d0c7e0003f
Rostacia <rostacia_lewis@hotmail.com>
2024-11-14 15:19:59 -0500

    Change countBits function using append reducing no. of lines of code

--- CountingBits.py
+++ CountingBits.py
@@ -1,14 +1,12 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = [0] * (n + 1)
-
+        ans = [0] 
         for i in range(1, n + 1):
             count = 0
-            index = i
             while i:
                 count += i % 2
                 i = i // 2
-            ans[index] = count
+            ans.append(count)
         return ans
     
         # dp = [0] * (n + 1)
