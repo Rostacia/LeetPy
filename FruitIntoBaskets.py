2f18c574ee5d7ad36f98de411eb2fbf024089ce6
Rostacia <rostacia_lewis@hotmail.com>
2024-11-15 13:43:34 -0500

    Upload another way of solving fruit into baskets

--- FruitIntoBaskets.py
+++ FruitIntoBaskets.py
@@ -3,11 +3,12 @@ class Solution:
         if not fruits:
             return 0
         l = 0
-        maxF = 0
-        fruitBask = defaultdict(int)
+        maxF = 1
+        fruitBask = {}
+        fruitBask[fruits[l]] = 1
 
-        for r in range(len(fruits)):
-            fruitBask[fruits[r]] += 1
+        for r in range(1, len(fruits)):
+            fruitBask[fruits[r]] = fruitBask.get(fruits[r], 0) + 1
 
             while len(fruitBask) > 2:
                 fruitBask[fruits[l]] -= 1
@@ -16,3 +17,20 @@ class Solution:
                 l += 1
             maxF = max(maxF, 1 + r - l)
         return maxF
+       
+        # if not fruits:
+        #     return 0
+        # l = 0
+        # maxF = 0
+        # fruitBask = defaultdict(int)
+
+        # for r in range(len(fruits)):
+        #     fruitBask[fruits[r]] += 1
+
+        #     while len(fruitBask) > 2:
+        #         fruitBask[fruits[l]] -= 1
+        #         if fruitBask[fruits[l]] == 0:
+        #             del fruitBask[fruits[l]]
+        #         l += 1
+        #     maxF = max(maxF, 1 + r - l)
+        # return maxF
